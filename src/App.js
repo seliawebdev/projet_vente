4f3f2950625e46f64994df2d028e0494fde8bc95
Céline Perriard <perriard.1004.student@ifosupwavre.be>
2020-10-19 19:47:46 +0200

    création du projet avec create-react-app et ajout des fichiers de la homepage

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+//import logo from './logo.svg';
+import './App.css';
+import HomePage from './homepage.component'
+
+
+
+function App() {
+  return (
+    <div>
+     <HomePage/>>
+    </div>
+  );
+}
+
+export default App;
