3dd05e41ce20c4cd1bad6844caf1f970542efbc9
Céline Perriard <perriard.1004.student@ifosupwavre.be>
2020-10-25 16:30:50 +0100

    Mise a jour de notre app avec une page shopping, apercus collection et item de collection

--- src/App.js
+++ src/App.js
@@ -1,60 +1,17 @@
 import React from 'react';
-import { Switch, Route, Link } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 //import logo from './logo.svg';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
-
-const chapeaux = () => (
-
-  <div>
-
-    <h1>Une page</h1>
-
-  </div>
-
-)
-
-
-const UnePage = props => {
-  return (
-
-  <div>
-
-    <Link to='/detail/0'>Détail zéro</Link>
-    <button onClick={ () => props.history.push('/detail/push')}>par push</button>
-    <h1>Une page</h1>
-
-  </div>
-
-)
-  }
-
-
-
-const AutrePage = props =>() => {
-  console.log(props)
-  return (
-
-
-<div>
-
-    <h1>Une autre page : {props.match.params.refArticle}</h1>
-
-  </div>
-
-)
-  }
+import ShopPage from './pages/shopping/shopping.component';
 
 function App() {
   return (
     <div>
       <Switch>
       <Route exact path='/' component={HomePage} />
-
-      <Route path='/chapeaux' component={chapeaux} />
+      <Route path='/Shopping' component={ShopPage} />
       </Switch>
-
-      <Route path="/detail/:refArticle" component={AutrePage} />
     </div>
   );
 }
---
+++ src/components/apercu-collection/apercu-collection.component.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+
+import CollectionItem from '../collection-item/collection-item.component'
+
+import './apercu-collection.styles.scss'
+
+
+
+const ApercuCollection = ({ titre, items}) => (
+
+  <div className='apercu-collection'>
+
+    <h1 className='titre'>{titre.toUpperCase()}</h1>
+
+    <div className='apercu'>
+
+      {
+
+        items
+            .filter((item, idx) => idx < 4)
+            .map(({id, ...otherItemPorps}) => (
+
+                <CollectionItem key={id} {...otherItemPorps}/>
+
+          //<div key={item.id}> {item.nom}</div>
+
+        ))
+
+      }
+
+    </div>
+
+  </div>
+
+
+
+)
+
+
+
+export default ApercuCollection
